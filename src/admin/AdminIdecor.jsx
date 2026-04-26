cf24f6ca169dfe648e35e65d1ed7c1ba0ccacb8c
Gabriel Fernando Correa <gabrielfcorrea3@gmail.com>
2026-04-26 12:09:02 -0300

    cambios en vista Idecor

--- src/admin/AdminIdecor.jsx
+++ src/admin/AdminIdecor.jsx
@@ -29,6 +29,7 @@ function AdminIdecor() {
                 Buscar informacion de Catastro
             </button>
             <hr />
+            <hr />
 
             <button
                 onClick={() => {
@@ -43,6 +44,7 @@ function AdminIdecor() {
                 Descargar informe impositivo
             </button>
             <hr />
+            <hr />
             <button
                 onClick={() => {
                     navigator.clipboard.writeText(data.idecor.cuenta)
@@ -56,6 +58,7 @@ function AdminIdecor() {
                 Ver estado impositivo
             </button>
             <hr />
+            <hr />
             <button
                 onClick={() =>
                     window.open(`${API_URL}/idecor/kml/${numero}`, "_blank")
@@ -68,24 +71,36 @@ function AdminIdecor() {
 
             {data?.idecor && (
                 <div className="mt-4 bg-gray-100 p-4 rounded space-y-2">
-                    <p><b>Cuenta:</b> {data.idecor.cuenta}</p>
-                    <p><b>Nomenclatura:</b> {data.idecor.nomenclatura}</p>
-                    <p><b>Designación:</b> {data.idecor.designacion}</p>
-                    <p><b>Tipo:</b> {data.idecor.tipo_inmueble}</p>
-                    <p><b>Estado:</b> {data.idecor.estado}</p>
-                    <p><b>Sup. Terreno:</b> {data.idecor.superficie_terreno} m²</p>
-                    <p><b>Sup. Edificada:</b> {data.idecor.superficie_mejoras} m²</p>
-                    <p><b>Valuación:</b> ${data.idecor.valuacion_total}</p>
-
-
-                    {/* BOTONES */}
-
-
-
+                    <p><b>Cuenta:</b> </p>
+                    <p><b>Nomenclatura:</b> </p>
+                    <p><b>Designación:</b> </p>
+                    <p><b>Tipo:</b> </p>
+                    <p><b>Estado:</b> </p>
+                    <p><b>Sup. Terreno:</b>  m²</p>
+                    <p><b>Sup. Edificada:</b>  m²</p>
+                    <p><b>Valuación:</b> $</p>
                 </div>
             )}
+
+
         </div>
     )
 }
 
 export default AdminIdecor
+
+
+            // {data?.idecor && (
+            //     <div className="mt-4 bg-gray-100 p-4 rounded space-y-2">
+            //         <p><b>Cuenta:</b> {data.idecor.cuenta}</p>
+            //         <p><b>Nomenclatura:</b> {data.idecor.nomenclatura}</p>
+            //         <p><b>Designación:</b> {data.idecor.designacion}</p>
+            //         <p><b>Tipo:</b> {data.idecor.tipo_inmueble}</p>
+            //         <p><b>Estado:</b> {data.idecor.estado}</p>
+            //         <p><b>Sup. Terreno:</b> {data.idecor.superficie_terreno} m²</p>
+            //         <p><b>Sup. Edificada:</b> {data.idecor.superficie_mejoras} m²</p>
+            //         <p><b>Valuación:</b> ${data.idecor.valuacion_total}</p>
+            //     </div>
+            // )}
+
+
